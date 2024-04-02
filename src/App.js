72b5a9d308b5abdc5cb133f68ae423609ae1aa5f
goldyloop <g0583260@gmail.com>
2024-04-02 15:05:28 +0300

    mui

--- src/App.js
+++ src/App.js
@@ -1,22 +1,55 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useState } from 'react';
+import ButtonGroup from '@mui/material/ButtonGroup'
+import Button from '@mui/material/Button'
+import Checkbox from '@mui/material/Checkbox'
+import FormControlLabel from '@mui/material/FormControlLabel'
+import { TextField } from '@mui/material';
+
+import SaveIcon from '@mui/icons-material/Save'
+import DeleteIcon from '@mui/icons-material/Delete'
+
+function CheckboxExemple(){
+  const [checked,setChecked]=useState(true)
+  return (
+    <FormControlLabel
+     control={
+      <Checkbox
+      checked={checked}
+      onChange={(e)=>setChecked(e.target.checked)}
+      icon={<DeleteIcon/>}
+      checkedIcon={<SaveIcon/>}
+      inputProps={{
+        'aria-label':'secondary checkbox'
+      }}
+    />}
+    label="testing checkbox"
+    />
+  )
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <TextField variant='filled'
+        color='secondary'
+        type="time"
+        label="the time"
+        />
+        
+
+        <CheckboxExemple/>
+        <ButtonGroup>
+          <Button variant="contained" color="primary" startIcon={<SaveIcon />} >
+            Save
+          </Button>
+          <Button variant="contained" color="secondary" startIcon={<DeleteIcon />} >
+             Discard
+          </Button>
+        </ButtonGroup>
+
       </header>
     </div>
   );
